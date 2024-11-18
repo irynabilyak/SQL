43e13ffdd29b9ad8280c9d52cf9be2b84dedd98b
Iryna Bilyak <161013564+irynabilyak@users.noreply.github.com>
2024-11-18 12:38:55 +0200

    Add files via upload

---
+++ sql_practice/SELECT_cases.sql
@@ -0,0 +1,50 @@
+SELECT
+	1 AS nmb_1
+	,2	AS nmb_2
+	,5+5 AS nmb_3;
+
+SELECT *
+FROM salaries;
+
+
+SELECT 
+	COUNT (*)			AS	cnt_all
+	, COUNT (exp_level)	AS	cnt_level
+	, COUNT (DISTINCT exp_level)
+FROM salaries
+LIMIT 10;
+
+SELECT 
+	AVG(salary_in_usd)			AS	salary_avg
+	, MAX(salary_in_usd)		AS	salary_max
+	, MIN(salary_in_usd)		AS	salary_min
+FROM salaries
+WHERE year=2023
+LIMIT 10;
+
+
+SELECT 
+	year
+	, exp_level
+	, salary_in_usd
+	, salary_in_usd*40	AS salary_in_uah
+FROM salaries
+LIMIT 10;
+
+
+SELECT 
+	year
+	, exp_level
+	, salary_in_usd
+	, salary_in_usd*40	AS salary_in_uah
+	--'SE' - Senior
+	--'MI' - Middle
+	-- Other
+	, CASE
+		WHEN exp_level='SE'
+		THEN 'Senior'
+		WHEN exp_level='MI'
+		THEN 'Middle'
+		ELSE 'Others' END	AS full_level 
+FROM salaries
+LIMIT 10;
