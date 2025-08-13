34154475b2128fcebb36038392458fdec9847cb1
Iryna Bilyak <161013564+irynabilyak@users.noreply.github.com>
2025-08-13 12:02:31 +0300

    Update and rename Average Time of Process per Machine to Ave_Time_per_Machine.sql

--- sql_practice/Ave_Time_per_Machine.sql
+++ sql_practice/Ave_Time_per_Machine.sql
@@ -1,4 +1,4 @@
-Table: Activity
+/*Table: Activity
 
 +----------------+---------+
 | Column Name    | Type    |
@@ -63,7 +63,7 @@ Explanation:
 There are 3 machines running 2 processes each.
 Machine 0's average time is ((1.520 - 0.712) + (4.120 - 3.140)) / 2 = 0.894
 Machine 1's average time is ((1.550 - 0.550) + (1.420 - 0.430)) / 2 = 0.995
-Machine 2's average time is ((4.512 - 4.100) + (5.000 - 2.500)) / 2 = 1.456
+Machine 2's average time is ((4.512 - 4.100) + (5.000 - 2.500)) / 2 = 1.456 */
 
 WITH machine_id_process_id_start_time AS (
     SELECT a.machine_id AS "machine_id", a.process_id AS "process_id", a.timestamp AS "timestamp"
